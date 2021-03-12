37e28a25283e86c3fc54059c9178a7bb2c150f32
Ishpreet21698 <ishpreetsinghbhayana@gmail.com>
2021-03-12 20:12:07 +0530

    some updates to the code

--- client.c
+++ client.c
@@ -1,11 +1,22 @@
-#include <sys/socket.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <pthread.h>
 
+
 char msg[500];
+char name[32];
+
+void str_overwrite_stdout() {
+  printf("%s", "> ");
+  fflush(stdout);
+}
 
 void *recvmg(void *my_sock)
 {
@@ -25,7 +36,11 @@ int main(int argc,char *argv[]){
 	char send_msg[500];
 	struct sockaddr_in ServerIp;
 	char client_name[100];
-	strcpy(client_name, argv[1]);
+
+	printf("Please enter your name: ");
+	fgets(name, 32, stdin);
+
+	strcpy(client_name, name);
 	sock = socket( AF_INET, SOCK_STREAM,0);
 	ServerIp.sin_port = htons(8888);
 	ServerIp.sin_family= AF_INET;
@@ -35,17 +50,31 @@ int main(int argc,char *argv[]){
 	
 	//creating a client thread which is always waiting for a message
 	pthread_create(&recvt,NULL,(void *)recvmg,&sock);
+
+	printf("=== WELCOME TO THE CHATROOM ===\n");
 	
 	//ready to read a message from console
 	while(fgets(msg,500,stdin) > 0) {
+		str_overwrite_stdout();
 		strcpy(send_msg,client_name);
 		strcat(send_msg,":");
 		strcat(send_msg,msg);
 		len = write(sock,send_msg,strlen(send_msg));
+		if (strcmp(msg, "exit") == 0){
+			printf("\nBye\n");
+			break;
+		}
 		if(len < 0) 
 			printf("\n message not sent \n");
 			
 	}
+
+	//while (1){
+	//	if(flag){
+	//		printf("\nBye\n");
+	//		break;
+    //}
+	//}
 	
 	//thread is closed
 	pthread_join(recvt,NULL);
